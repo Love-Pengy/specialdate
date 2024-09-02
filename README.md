39e720d4090443d41cf6d9dbdbcea3b70ffde879
bee <afrazierboy2001@gmail.com>
2024-09-01 21:59:04 -0400

    [FEAT] add affirmations and read me

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # specialdate
+
+Gives you that little motivation you need in the morning 
+
+![Demo](./assets/IMG_3771.jpg)
+
+## Build Requirements
++ Arduino IDE 
++ [DS3231](https://github.com/NorthernWidget/DS3231/blob/master/Documentation/Time-Set.md#void-setdowbyte-dow)
++ [LiquidCrystal_I2C](https://github.com/lucasmaziero/LiquidCrystal_I2C)
+
+## Hardware 
+- Arduino Nano 
+- 16x2 LCD
+- DS3231 RTC
+
+## To Use 
+- Change the CALIB* macros to your current date and flash the arduino 
+- after that coment out the rtc.set* in the setup function
+- flash again 
+- you're good to go 
--- SpecialTime/SpecialTime.ino
+++ SpecialTime/SpecialTime.ino
@@ -1,6 +1,7 @@
 #include <Wire.h>
 #include <time.h>
 #include <DS3231.h>
+#include <stdlib.h>
 #include <LiquidCrystal_I2C.h>
 DS3231 rtc;
 DS3231 futureRtc;
@@ -9,20 +10,47 @@ RTClib futureRtcLib;
 
 
 #define CALIBSECONDS 0
-#define CALIBMINUTE 55
-#define CALIBHOUR 10
-#define CALIBDOW 5
-#define CALIBDATE 4
-#define CALIBMONTH 7
+#define CALIBMINUTE 59
+#define CALIBHOUR 9
+#define CALIBDOW 1 // 1 -7 Sunday = 1
+#define CALIBDATE 1
+#define CALIBMONTH 9
 #define CALIBYEAR 24
-DateTime future = DateTime(2025, 1, 3, CALIBHOUR, CALIBMINUTE, 0);
+
+static char affirmations[24][17] = {
+  "You Got This <33", 
+  "   Keep Going!  ", 
+  "  Never Give Up ",  
+  "Prove Them Wrong", 
+  "  Take The Risk ", 
+  "You Can Do This ", 
+  "Do Great Today! ", 
+  "You Are Evolving", 
+  "You Are Awesome!", 
+  "Good Luck Today!", 
+  "Big Work Big Win", 
+  "Flip The Page   ", 
+  "Be Lovely Today!", 
+  "  Smile Today!  ", 
+  "Storms Dont Last", 
+  "Try Everything  ", 
+  "You are Strong! ", 
+  "Obtain Progress ", 
+  " Be Persistent! ", 
+  "  Be Positive!  ", 
+  "  Be Patient!   ", 
+  " You Are Valued ", 
+  "Take Small Steps",
+  "  Remember Why  ", 
+};
+
+DateTime future = DateTime(2025, 1, 3, 0, 0, 0);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
   //Serial.begin(57600);
   Wire.begin();
   delay(500);
-  
   rtc.setClockMode(true); //set to 12 hour clock
   rtc.setSecond(CALIBSECONDS);
   rtc.setMinute(CALIBMINUTE); //set minute 
@@ -31,9 +59,9 @@ void setup() {
   rtc.setDate(CALIBDATE); //set day
   rtc.setMonth(CALIBMONTH); //set month
   rtc.setYear(CALIBYEAR); //set year
-  
   lcd.init();
   lcd.backlight();
+  srand(time(NULL));
 }
 
 void loop() {
@@ -45,12 +73,10 @@ void loop() {
   time_t futureTime = future.unixtime();
   time_t calcTime = futureTime - nowTime;
   int days = calcTime/60/60/24;
-  Serial.println(calcTime);
+  //Serial.println(rtc.getYear());
   lcd.setCursor(0, 0);
   snprintf(buff, 17, "%d%s", days, " Days Left!!!");
   lcd.print(buff);
   lcd.setCursor(0, 1);
-  lcd.print("You Got This <33");
-
+  lcd.print(affirmations[now.day()%24]);
 }
-
